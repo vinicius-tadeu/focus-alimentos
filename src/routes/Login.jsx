becb35ebb6fc29387b4bb3ab3e34b81b41243b82
vinicius-tadeu <viniciustadeusores@gmail.com>
2023-05-13 03:55:36 -0300

    teste

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -43,7 +43,7 @@ function Login() {
     });
     console.log(usuario, document.id);
     armazenar(usuario, document.id);
-    window.location.href = "/home";
+    window.location.assign("/home");
   }
 
   async function authLogin() {
