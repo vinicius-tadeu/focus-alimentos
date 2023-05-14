4233d4b04b43e7302f838256e8777665837dc51c
vinicius-tadeu <viniciustadeusores@gmail.com>
2023-05-14 13:26:43 -0300

    Input para buscar alimentos criado

--- src/routes/Alimentos.jsx
+++ src/routes/Alimentos.jsx
@@ -1,22 +1,59 @@
 import DefaultPage from "../components/DefaultPage";
 import data from "../services/utis.json";
+import { useMemo, useState } from "react";
 import "./Alimentos.css";
 export default function Alimentos() {
+  const [busca, setBusca] = useState("");
+  console.log(data);
+  const dataFilter = useMemo(()=>{
+    const lowerBusca = busca.toLowerCase();
+    return data.filter((a) =>
+      a.description.toLowerCase().includes(lowerBusca)
+    );
+  },[busca]) 
   return (
     <>
-    <DefaultPage/>
+      <DefaultPage />
       <h1 className="titleFoods">Alimentos</h1>
+      <div className="wrapperBusca">
+        <h2>Buscar</h2>
+        <input
+          type="text"
+          value={busca}
+          onChange={(e) => {
+            setBusca(e.target.value);
+          }}
+        />
+      </div>
       <div className="wrapperFood">
-        {data?.map((i) => {
+        {dataFilter?.map((i) => {
           return (
             <div key={i.id} className="foodCard">
               <p className="cardCount">{i.id}</p>
               <h2>{i.description}</h2>
               <p>Categoria: {i.category}</p>
-              <p>Proteínas: {Math.round(i.protein_g)?Math.round(i.protein_g)+"g":"*"}</p>
-              <p>Carboidratos: {Math.round(i.carbohydrate_g)?Math.round(i.carbohydrate_g)+"g":"*"}</p>
-              <p>Kcal: {Math.round(i.energy_kcal)?Math.round(i.energy_kcal)+"kcal":"*"}</p>
-              <p>Kj: {Math.round(i.energy_kj)?Math.round(i.energy_kcal)+"kj":"*"}</p>
+              <p>
+                Proteínas:{" "}
+                {Math.round(i.protein_g) ? Math.round(i.protein_g) + "g" : "*"}
+              </p>
+              <p>
+                Carboidratos:{" "}
+                {Math.round(i.carbohydrate_g)
+                  ? Math.round(i.carbohydrate_g) + "g"
+                  : "*"}
+              </p>
+              <p>
+                Kcal:{" "}
+                {Math.round(i.energy_kcal)
+                  ? Math.round(i.energy_kcal) + "kcal"
+                  : "*"}
+              </p>
+              <p>
+                Kj:{" "}
+                {Math.round(i.energy_kj)
+                  ? Math.round(i.energy_kcal) + "kj"
+                  : "*"}
+              </p>
             </div>
           );
         })}
